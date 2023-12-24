483f54fef4c18a4ad3e45480de7cab341808650f
h.Hosseini <hmd.sepehry@gmail.com>
2023-12-24 16:07:53 +0330

    exportCsv button implemented successfully

--- src/pages/dashboard/dashborad.routes.tsx
+++ src/pages/dashboard/dashborad.routes.tsx
@@ -1,56 +1,64 @@
-import React, { lazy, LazyExoticComponent } from 'react';
-import { IllustrationViewer } from 'src/components/static/illustration-viewer';
+import React, { lazy, LazyExoticComponent } from "react";
+import { IllustrationViewer } from "src/components/static/illustration-viewer";
 
-const MainPage = lazy(() => import('src/pages/dashboard/main'));
-const Statistics = lazy(() => import('src/pages/dashboard/statistics'));
-const BranchList = lazy(() => import('src/pages/dashboard/branch/branch-list'));
-const BranchPage = lazy(() => import('src/pages/dashboard/branch/branch-page'));
-const Activities = lazy(() => import('src/pages/dashboard/activities'));
-const QrCode = lazy(() => import('src/pages/dashboard/qrcode'));
-const Entrance = lazy(() => import('src/pages/dashboard/entrance'));
-const EntranceHistory = lazy(() => import('src/pages/dashboard/entrance-history'));
-const FloorPlan = lazy(() => import('src/pages/dashboard/floorplan'));
-const Developers = lazy(() => import('src/pages/dashboard/developers'));
-const Restricted = lazy(() => import('src/pages/dashboard/restricted'));
-const RestrictedHistory = lazy(() => import('src/pages/dashboard/restricted-history'));
-const RestrictedSettings = lazy(() => import('src/pages/dashboard/restricted-settings'));
-const Users = lazy(() => import('src/pages/dashboard/users'));
-const AddUser = lazy(() => import('src/pages/dashboard/add-user-new'));
-const licensePlate = lazy(() => import('src/pages/dashboard/license-plate'));
-const licensePlateHistory = lazy(() => import('src/pages/dashboard/license-plate-history'));
-const camerasList = lazy(() => import('src/pages/dashboard/cameras'));
-const boardsList = lazy(() => import('src/pages/dashboard/boards'));
+const MainPage = lazy(() => import("src/pages/dashboard/main"));
+const Statistics = lazy(() => import("src/pages/dashboard/statistics"));
+const BranchList = lazy(() => import("src/pages/dashboard/branch/branch-list"));
+const BranchPage = lazy(() => import("src/pages/dashboard/branch/branch-page"));
+const Activities = lazy(() => import("src/pages/dashboard/activities"));
+const QrCode = lazy(() => import("src/pages/dashboard/qrcode"));
+const Entrance = lazy(() => import("src/pages/dashboard/entrance"));
+const EntranceHistory = lazy(
+  () => import("src/pages/dashboard/entrance-history")
+);
+const FloorPlan = lazy(() => import("src/pages/dashboard/floorplan"));
+const Developers = lazy(() => import("src/pages/dashboard/developers"));
+const Restricted = lazy(() => import("src/pages/dashboard/restricted"));
+const RestrictedHistory = lazy(
+  () => import("src/pages/dashboard/restricted-history")
+);
+const RestrictedSettings = lazy(
+  () => import("src/pages/dashboard/restricted-settings")
+);
+const Users = lazy(() => import("src/pages/dashboard/users"));
+const AddUser = lazy(() => import("src/pages/dashboard/add-user"));
+const licensePlate = lazy(() => import("src/pages/dashboard/license-plate"));
+const licensePlateHistory = lazy(
+  () => import("src/pages/dashboard/license-plate-history")
+);
+const camerasList = lazy(() => import("src/pages/dashboard/cameras"));
+const boardsList = lazy(() => import("src/pages/dashboard/boards"));
 
 export enum RouteName {
-  BRANCH_LIST = 'branch-list',
+  BRANCH_LIST = "branch-list",
   BRANCH = `branch-list/:id`,
-  SERVICE_LIST = 'services',
-  SERVICE = 'services/:id?',
-  STATISTICS = 'statistics',
-  ACTIVITIES = 'activities',
-  RESTRICTED = 'restricted',
-  RESTRICTED_HISTORY = 'restricted/history',
-  RESTRICTED_CONFIG = 'restricted/config',
-  SUPPORT = 'support',
-  QR_CODE = 'qrcode',
-  ENTRANCE = 'entrance',
-  ENTRANCE_HISTORY = 'entrance/history',
-  FLOOR_PLAN = 'floor-plan',
-  DEVELOPERS = 'developers',
-  USERS = 'users',
-  ADD_USER = 'users/add',
-  USER = 'user/:id',
-  MAIN = 'dashboard',
-  LICENSE_PLATE = 'license-plate',
-  LICENSE_PLATE_HISTORY = 'license-plate/history',
-  CAMERA_LIST = 'infrastructure/cameras',
-  BOARDS_LIST = 'infrastructure/boards',
+  SERVICE_LIST = "services",
+  SERVICE = "services/:id?",
+  STATISTICS = "statistics",
+  ACTIVITIES = "activities",
+  RESTRICTED = "restricted",
+  RESTRICTED_HISTORY = "restricted/history",
+  RESTRICTED_CONFIG = "restricted/config",
+  SUPPORT = "support",
+  QR_CODE = "qrcode",
+  ENTRANCE = "entrance",
+  ENTRANCE_HISTORY = "entrance/history",
+  FLOOR_PLAN = "floor-plan",
+  DEVELOPERS = "developers",
+  USERS = "users",
+  ADD_USER = "users/add",
+  USER = "user/:id",
+  MAIN = "dashboard",
+  LICENSE_PLATE = "license-plate",
+  LICENSE_PLATE_HISTORY = "license-plate/history",
+  CAMERA_LIST = "infrastructure/cameras",
+  BOARDS_LIST = "infrastructure/boards",
 }
 
 export interface RouteProps {
   path: RouteName;
   exact: boolean;
-  routeTitle?: string,
+  routeTitle?: string;
   component: LazyExoticComponent<() => JSX.Element> | (() => JSX.Element);
 }
 
@@ -68,7 +76,10 @@ export const routes: RouteProps[] = [
   {
     path: RouteName.SERVICE,
     component: () => (
-      <IllustrationViewer illustration={'comingSoon.svg'} title={'general.comingSoon'} />
+      <IllustrationViewer
+        illustration={"comingSoon.svg"}
+        title={"general.comingSoon"}
+      />
     ),
     exact: true,
   },
@@ -80,32 +91,38 @@ export const routes: RouteProps[] = [
   {
     path: RouteName.ACTIVITIES,
     component: () => (
-      <IllustrationViewer illustration={'comingSoon.svg'} title={'general.comingSoon'} />
+      <IllustrationViewer
+        illustration={"comingSoon.svg"}
+        title={"general.comingSoon"}
+      />
     ),
     exact: true,
   },
   {
     path: RouteName.RESTRICTED,
     component: Restricted,
-    routeTitle: 'restricted',
+    routeTitle: "restricted",
     exact: true,
   },
   {
     path: RouteName.RESTRICTED_HISTORY,
     component: RestrictedHistory,
-    routeTitle: 'restrictedHistory',
+    routeTitle: "restrictedHistory",
     exact: true,
   },
   {
     path: RouteName.RESTRICTED_CONFIG,
     component: RestrictedSettings,
-    routeTitle: 'restrictedConfig',
+    routeTitle: "restrictedConfig",
     exact: true,
   },
   {
     path: RouteName.SUPPORT,
     component: () => (
-      <IllustrationViewer illustration={'comingSoon.svg'} title={'general.comingSoon'} />
+      <IllustrationViewer
+        illustration={"comingSoon.svg"}
+        title={"general.comingSoon"}
+      />
     ),
     exact: true,
   },
@@ -117,13 +134,13 @@ export const routes: RouteProps[] = [
   {
     path: RouteName.ENTRANCE,
     component: Entrance,
-    routeTitle: 'faceRec',
+    routeTitle: "faceRec",
     exact: true,
   },
   {
     path: RouteName.ENTRANCE_HISTORY,
     component: EntranceHistory,
-    routeTitle: 'faceRecHistory',
+    routeTitle: "faceRecHistory",
     exact: true,
   },
   {
@@ -134,56 +151,59 @@ export const routes: RouteProps[] = [
   {
     path: RouteName.DEVELOPERS,
     component: () => (
-      <IllustrationViewer illustration={'comingSoon.svg'} title={'general.comingSoon'} />
+      <IllustrationViewer
+        illustration={"comingSoon.svg"}
+        title={"general.comingSoon"}
+      />
     ),
     exact: true,
   },
   {
     path: RouteName.MAIN,
     component: MainPage,
-    routeTitle: 'dashboard',
+    routeTitle: "dashboard",
     exact: true,
   },
   {
     path: RouteName.USERS,
     component: Users,
-    routeTitle: 'employees',
+    routeTitle: "employees",
     exact: true,
   },
   {
     path: RouteName.USER,
     component: AddUser,
-    routeTitle: 'profile',
+    routeTitle: "profile",
     exact: true,
   },
   {
     path: RouteName.ADD_USER,
     component: AddUser,
-    routeTitle: 'addUser',
+    routeTitle: "addUser",
     exact: true,
   },
   {
     path: RouteName.LICENSE_PLATE,
     component: licensePlate,
-    routeTitle: 'licensePlate',
+    routeTitle: "licensePlate",
     exact: true,
   },
   {
     path: RouteName.LICENSE_PLATE_HISTORY,
     component: licensePlateHistory,
-    routeTitle: 'licensePlateHistory',
+    routeTitle: "licensePlateHistory",
     exact: true,
   },
   {
     path: RouteName.CAMERA_LIST,
     component: camerasList,
-    routeTitle: 'camerasList',
+    routeTitle: "camerasList",
     exact: true,
   },
   {
     path: RouteName.BOARDS_LIST,
     component: boardsList,
-    routeTitle: 'boardsList',
+    routeTitle: "boardsList",
     exact: true,
   },
 ];
